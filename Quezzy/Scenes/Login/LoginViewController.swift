970be5bf6f199e8810eea38fc5e39036bc4c502f
Nikita Aleksejevas <nik.aleksejev@gmail.com>
2023-01-27 12:21:50 +0200

    setted UI for loginVC and added outlets and actions

--- Quezzy/Scenes/Login/LoginViewController.swift
+++ Quezzy/Scenes/Login/LoginViewController.swift
@@ -8,22 +8,24 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    //MARK: -Outlets
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    //MARK: -Actions
+    @IBAction private func loginTapped(_ sender: Any) {
     }
-    */
-
+    
+    
+    @IBAction private func forgotPasswordTapped(_ sender: Any) {
+    }
+    
 }
