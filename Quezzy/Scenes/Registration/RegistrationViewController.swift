618e2e456a91a3a9e7ea9903fb4c8ddd640a18de
Nikita Aleksejevas <nik.aleksejev@gmail.com>
2023-01-30 12:05:27 +0200

    setted ui for registrationVC, added outlets and actions

--- Quezzy/Scenes/Registration/RegistrationViewController.swift
+++ Quezzy/Scenes/Registration/RegistrationViewController.swift
@@ -8,22 +8,26 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
-
+    
+    //MARK: -Outlets
+    
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var confirmPasswordTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: -Actions
+    
+    @IBAction private func signupTapped(_ sender: Any) {
     }
-    */
-
+    
+    
 }
